40dfff5db4106d348f4f0072508194fbb2421d94
Vamshi Krishna <guduru.vkrishna.civ10@itbhu.ac.in>
2018-02-20 15:15:11 +0530

    Initial Commit

---
+++ VKTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VKTableView
+//
+//  Created by Vamshi Krishna on 20/02/18.
+//  Copyright © 2018 Vamshi Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
